110d824ddf0e9a6ec0dcf33007479594aa9d1dff
João Monteiro <142106001+pedromontesi@users.noreply.github.com>
2025-10-11 20:26:17 -0300

    created array-foreach-exemple

---
+++ array-foreach-exemple/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$dados = [
+    'nome' => 'João',
+    'idade' => 22,
+    'email' => 'exemplo@email.com',
+    'ativo' => true,
+    'item' => 'Teclado',
+];
+
+foreach($dados as $key => $dado){
+	
+	print_r( "$key => $dado \n");
+	
+	
+}
+	
+
+
