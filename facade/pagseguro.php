2cee4d6a5be85d7638495ca68d6a8ae139b4ac6f
João Monteiro <142106001+pedromontesi@users.noreply.github.com>
2025-10-28 16:51:52 -0300

    refactor PagSeguroFacade class structure

--- facade/pagseguro.php
+++ facade/pagseguro.php
@@ -1,32 +1,43 @@
 <?php
-class PagSeguroFacade {
-private $request;
-public function __construct( $currency ) {
-$this->request = new PagSeguroPaymentRequest();
-$this->request->setCurrency( $currency );
-}
-public function addItem($product, $amount) {
-$item = new PagSeguroItem;
-$item->setId( $product->id );
-$item->setDescription( $product->description );$item->setQuantity( $amount );
-$item->setAmount( $product->price );
-$this->request->addItem($item);
-}
-public function setCustomer($customer) {
-$address = new PagSeguroAddress;
-$address->setPostalCode( $customer->postal );
-$address->setStreet( $customer->address );
-$address->setNumber( $customer->number );
-$address->setDistrict( $customer->neighborhood );
-$address->setCity( $customer->city );
-$address->setState( $customer->state );
-$this->request->setShippingAddress($address);
-$sender = new PagSeguroSender;
-$sender->setName( trim($customer->name) );
-$sender->setEmail( trim($customer->email) );
-$this->request->setSender($sender);
-}
-public function process() {
+
+class PagSeguroFacade
+{
+    private $request;
+
+    public function __construct($currency)
+    {
+        $this->request = new PagSeguroPaymentRequest();
+        $this->request->setCurrency($currency);
+    }
+
+    public function addItem($product, $amount)
+    {
+        $item = new PagSeguroItem;
+        $item->setId($product->id);
+        $item->setDescription($product->description);
+        $item->setQuantity($amount);
+        $item->setAmount($product->price);
+        $this->request->addItem($item);
+    }
+
+    public function setCustomer($customer)
+    {
+        $address = new PagSeguroAddress;
+        $address->setPostalCode($customer->postal);
+        $address->setStreet($customer->address);
+        $address->setNumber($customer->number);
+        $address->setDistrict($customer->neighborhood);
+        $address->setCity($customer->city);
+        $address->setState($customer->state);
+        $this->request->setShippingAddress($address);
+        $sender = new PagSeguroSender;
+        $sender->setName(trim($customer->name));
+        $sender->setEmail(trim($customer->email));
+        $this->request->setSender($sender);
+    }
+
+    public function process()
+    {
 // ...
-}
+    }
 }
